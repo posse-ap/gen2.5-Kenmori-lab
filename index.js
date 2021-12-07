19326ab7e3e94c0142c885b58d52a23b8a3bb1bb
Kensuke Morimoto <morimotokensuke0104@gmail.com>
2021-12-07 14:51:44 +0900

    選択肢を一つしか押せないようにする

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 'use strict';
-    const blue = document.getElementById('true-choice');
+    const trueBlue = document.getElementById('true-choice');
+    const falseRed1 = document.getElementById('wrong-choice1')
+    const falseRed2 = document.getElementById('wrong-choice2')
     // 正解をクリックした時の処理
-    blue.addEventListener('click', () => {
+    trueBlue.addEventListener('click', () => {
         const answerBox1 = document.getElementById('result-true');
-
-        blue.classList.add('button1');
+        trueBlue.classList.add('button1');
         answerBox1.classList.add('result-true');
+        falseRed1.classList.add('unClick');
+        falseRed2.classList.add('unClick');
     } , false);
     // addEventListener 使い方 対象要素.addEventListener( 種類, 関数, false )
     // クリックした時に正解を青くして、正解のボックスを出す
-
-    const red = document.getElementById('wrong-choice')
+    
     // 不正解をクリックしたときの処理
-    red.addEventListener('click',() => {
+    falseRed1.addEventListener('click',() => {
         const answerBox2 = document.getElementById('result-false');
-
-        blue.classList.add('button1');
-        red.classList.add('button2');
+        trueBlue.classList.add('button1','unClick');
+        falseRed1.classList.add('button2');
+        falseRed2.classList.add('unClick');
         answerBox2.classList.add('result-true');
-    } )
+        
+    } , false);
+    falseRed2.addEventListener('click',() => {
+        const answerBox2 = document.getElementById('result-false');
+        trueBlue.classList.add('button1','unClick');
+        falseRed1.classList.add('unClick');
+        falseRed2.classList.add('button2');
+        answerBox2.classList.add('result-true');
+    } , false);
     // クリックをしたときに正解を青く、不正解を赤く、不正解のボックスを出す
+    // 繰り返しの時は文字列として認識してもらう
